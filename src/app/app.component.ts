a05bd9e745a02456e9c204b56805c50c84a70a3d
Uuurrrrriy / <ihnatchukud@gmail.com>
2020-04-17 00:30:30 +0300

    lesson1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {UserModel} from '../models/UserModel';
+import {UserService} from './services/user.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1> Hello {{msg}} </h1>
+    <app-user *ngFor="let user of users" [user]="user"></app-user>
+  `,
+  styles: [`h1 {background: silver}`]
 })
 export class AppComponent {
-  title = 'lesson1';
+  msg: 'users';
+  users: UserModel[];
+
+  constructor(private userService: UserService) {
+    // this.greeting();
+    this.userService.getUsers()
+      .subscribe(value => this.users = value);
+  }
+
+  // greeting(){
+  //   console.log('hello');
+  // }
 }
+
