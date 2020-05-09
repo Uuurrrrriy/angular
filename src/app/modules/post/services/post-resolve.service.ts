b35203d4bdcf83d2e31c91211915e3ab7c26c264
Uuurrrrriy / <ihnatchukud@gmail.com>
2020-05-09 20:39:01 +0300

    lesson4

--- src/app/modules/post/services/post-resolve.service.ts
+++ src/app/modules/post/services/post-resolve.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import {ActivatedRoute, ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
-import {PostModel} from '../../models/PostModel';
+
 import {Observable} from 'rxjs';
 import {PostService} from './post.service';
+import {PostModel} from '../../../../models/PostModel';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +12,10 @@ export class PostResolveService implements Resolve<PostModel[]>{
 
   constructor(private postService: PostService) { }
 
+  // @ts-ignore
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<PostModel> | Promise<PostModel> | PostModel {
     // const id = +route.paramMap.get('id');
-    let id = route.queryParamMap.get('idOfUser');
+    const id = route.queryParamMap.get('idOfUser');
     this.postService.getPostsByUserID(+id);
     return ;
   }
