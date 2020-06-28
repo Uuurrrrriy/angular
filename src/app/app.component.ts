8173264ac147cb12b05fca50b4e7e2f1241286b5
Uuurrrrriy / <ihnatchukud@gmail.com>
2020-06-28 21:45:06 +0300

    lesson5Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lesson2';
+  // state: any;
+  //
+  // loginForm: {login: string, password: string } = {
+  //   login: '',
+  //   password: ''
+  // };
+  //
+  // forma: FormGroup;
+
+  // constructor() {
+  //   this.forma = new FormGroup({
+  //     login: new FormControl('',
+  //       [Validators.required, Validators.maxLength(10), (control) => {
+  //         console.log(control);
+  //         if (control.value < 2) {
+  //           return {customError: true};
+  //         }
+  //       }]),
+  //     password: this.password
+  //   });
+  // }
+
+  // ChangeState(xxx: HTMLInputElement) {
+  //   console.log(xxx.value);
+  //   this.state = xxx.value;
+  // }
+  // @ts-ignore
+
+  // password: FormControl = new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z]{6,}')]);
+
+  // checkState(forma: FormGroup) {
+  //   console.log(forma);
+  // }
+
+  forma: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.forma = formBuilder.group({
+      login: ['', [Validators.required, Validators.pattern('[a-z]{6,}')]],
+      password: '123421324234'
+    });
+  }
+
 }
